55ec4c78427c2c030099e8cb48fc91f0253fa588
The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>
2020-11-05 16:49:37 -0600

    add msggen command

--- commands/msgGen.js
+++ commands/msgGen.js
@@ -5,9 +5,9 @@ module.exports = {
         if (args[0] == 'verification') {
             var msg = 'Welcome to hackTAMS! This server will be the main source of communication and information.\n' + 
                     'First you must verify invitation. You should have received a DM from Hacker Duck. ' +
-                    'Follow his instructions to get verified. Remember to use the name and email you used to register for hackTAMS.\n'
+                    'Follow his instructions to get verified. Remember to use the name and email you used to register for hackTAMS.\n' +
                     'Not registered? No worries, can register here: https://hacktams.org/register\n\n' + 
-                    'If you have any issues or questions regarding verification, please DM @Dylan';
+                    'If you have any issues or questions regarding verification, please DM <@611762884464082945>';
             console.log(msg);
             message.channel.send(msg);
         }
