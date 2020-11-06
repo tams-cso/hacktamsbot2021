699f3eeb3d86118a791fff8a6a2a5ab3d0e4a7a1
Michael Zhao <michaelzhao314@gmail.com>
2020-11-06 02:21:18 -0600

    Removed some useless functions/lines

--- index.js
+++ index.js
@@ -8,13 +8,9 @@ const client = new Discord.Client({ ws: { intents: Discord.Intents.ALL } });
 
 var tokens = new Map();
 
+// TODO: Add the generate events function
+
 const fs = require('fs');
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter((file) => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
 client.directMessage = new Discord.Collection();
 const dmFiles = fs.readdirSync('./directMessage/').filter((file) => file.endsWith('.js'));
 for (const file of dmFiles) {
@@ -68,43 +64,6 @@ client.on('message', (message) => {
                     'To verify your server invitation, please enter \nyour first and last name and email used to register for hackTAMS.\nEx: `Hacker Duck hackerduck@hacktams.org`'
                 );
         }
-        // else {
-        //     const args = message.content.slice(prefix.length).split(/\s+/)
-        //     const command = args.shift();
-
-        //     switch (command) {
-        //         case 'verify':
-        //             if (args.length == 1)
-        //                 client.directMessage.get('verify')
-        //                 .assignRole(message, args[0], roles, tokens, client.guilds.cache.get(servers.hackTAMS));
-        //             break;
-        //     }
-        // }
-        return;
-    }
-
-    // test/debug commands
-    if (message.content.startsWith('---')) {
-        const args = message.content.slice(3).split(/\s+/);
-        const command = args.shift();
-        message.delete();
-
-        switch (command) {
-            // case 'command': //give list of commands
-            //     message.channel.send();
-            //     break;
-            case 'msggen':
-                generateMessage(message, args);
-                break;
-            case 'evtgen':
-                for (var i = 1; i < 19; i++) {
-                    let item = data.events[i];
-                    message.channel
-                        .send(`${item[0]}\n${item[1]} ${item[2]} ${item[3]}\nLocation: ${item[4]}`)
-                        .then(message.react(':thumbsup:'));
-                }
-                break;
-        }
         return;
     }
 
@@ -114,15 +73,9 @@ client.on('message', (message) => {
     message.delete();
 
     switch (command) {
-        // case 'command': //give list of commands
-        //     message.channel.send();
-        //     break;
+        // TODO: Add 'help' command to list out all commands
         case 'msggen':
             generateMessage(message, args);
-            break;
-        case 'ping':
-            message.channel.send('pong!');
-            client.commands.get('ping').execute(message, args, data.roles);
             break;
         case 'hello':
             message.channel.send('Hello there!');
