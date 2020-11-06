fa47fd4d9ce3c0b69c2022d3ceb2f343f7f49855
Michael Zhao <michaelzhao314@gmail.com>
2020-11-06 04:02:24 -0600

    Make verification async function and extract selector

--- src/verification.js
+++ src/verification.js
@@ -1,7 +1,8 @@
-const { google } = require('googleapis');
+const { google, GoogleApis } = require('googleapis');
 const nodemailer = require('nodemailer');
 const Discord = require('discord.js');
 const keys = require('../config.json');
+const data = require('./data.json');
 
 const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
 
@@ -19,13 +20,14 @@ const transporter = nodemailer.createTransport({
 /**
  * Verifies a user by reading from the registration spreadsheet
  * then sending them an email
- * 
+ *
  * @param {Discord.Message} message The Discord Message object
  * @param {string[]} args Message arguments
- * @param {{}} tokens List of user auth tokens 
+ * @param {{}} tokens List of user auth tokens
  */
 module.exports = (message, args, tokens) => {
-    jwt.authorize((err, res) => {
+    jwt.authorize(async (err, res) => {
+        // Build request object
         const request = {
             spreadsheetId: '12GnU9CE3EftyUs8Uz_AQCWXGF09FKI-lwqh7YD3EDWc',
             ranges: ['A2:A', 'B2:B', 'C2:C', 'T2:T'],
@@ -33,70 +35,62 @@ module.exports = (message, args, tokens) => {
             auth: jwt,
         };
 
-        google
-            .sheets('v4')
-            .spreadsheets.get(request)
-            .then((data) => {
-                const cols = data.data.sheets[0].data;
-                for (var i = 0; i < cols[2].rowData.length; i++)
-                    if (
-                        cols[2].rowData[i].values[0].formattedValue.toLowerCase() ==
-                        args[2].toLowerCase()
-                    ) {
-                        if (
-                            cols[0].rowData[i].values[0].formattedValue.toLowerCase() ==
-                                args[0].toLowerCase() &&
-                            cols[1].rowData[i].values[0].formattedValue.toLowerCase() ==
-                                args[1].toLowerCase()
-                        ) {
-                            const temp = cols[3].rowData[i].values[0].formattedValue;
-                            const verCode = temp.substring(temp.length - 6);
-                            tokens["verCode"] = message.author.id;
-                            // console.log(temp.substring(temp.length-6));
-                            // console.log(message.author.id);
-                            //send eamil
-                            const mes =
-                                'Verification code for ' +
-                                message.author.username +
-                                ': ' +
-                                verCode +
-                                '\nDM Hacker Duck with your verification code to gain access to the hackTAMS server.\n\nIf this was not you, please contact dylanli@hacktams.org';
-                            const mailOptions = {
-                                // from: 'jamestheduck@hacktams.org',
-                                from: 'notifications@hacktams.org',
-                                to: args[2],
-                                subject: 'hackTAMS Discord Verification',
-                                text: mes,
-                                replyTo: 'dylanli@hacktams.org',
-                                // html: 'Embedded image: <img src="cid:somethingthatisunique"/>',
-                                // attachments: [{
-                                //     path: './directMessage/banner.png',
-                                //     cid: 'somethingthatisunique'
-                                // }]
-                            };
-                            transporter.sendMail(mailOptions, function (error, info) {
-                                if (error) console.log(error);
-                                else
-                                    console.log(
-                                        'Mail sent to ' +
-                                            args[2] +
-                                            ' User: ' +
-                                            message.author.username +
-                                            ' Code: ' +
-                                            verCode
-                                    );
-                            });
-                            message.author.send('Please check your email for a verification code');
-                        } else
-                            message.author.send(
-                                'Incorrect name or email: Please verify you have entered the correct name and email'
-                            );
-                        break;
-                    }
-                if (i == cols[2].rowData.length)
-                    message.author.send(
-                        'Registration not found: Please verify you have entered the correct name and email in the following format\nFirstName LastName Email\nEx: `Hacker Duck hackerduck@hacktams.org`'
-                    );
+        // Request spreadsheet data
+        const sheetData = await google.sheets('v4').spreadsheets.get(request);
+
+        // Extract columns from response
+        const cols = sheetData.data.sheets[0].data;
+
+        // Iterate through the rows and process auth tokens
+        for (var i = 0; i < cols[2].rowData.length; i++) {
+            // Continue if invalid first name, last name, or email
+            if (
+                cellValue(cols, i, 0).toLowerCase() !== args[0].toLowerCase() ||
+                cellValue(cols, i, 1).toLowerCase() !== args[1].toLowerCase() ||
+                cellValue(cols, i, 2).toLowerCase() !== args[2].toLowerCase()
+            ) {
+                continue;
+            }
+
+            // Extract last 6-characters as auth string
+            const temp = cellValue(cols, i, 3);
+            const verCode = temp.substring(temp.length - 6);
+
+            // Store that auth code
+            tokens['verCode'] = message.author.id;
+
+            // Send the email!
+            const mailOptions = {
+                from: 'notifications@hacktams.org',
+                to: args[2],
+                subject: 'hackTAMS Discord Verification',
+                text: `Verification code for ${message.author.username}: ${verCode}`,
+                replyTo: 'dylanli@hacktams.org',
+            };
+            transporter.sendMail(mailOptions, function (error) {
+                if (error) console.log(error);
+                else {
+                    console.log(`Mail sent to ${args[2]}`);
+                    console.log(`  | User: ${message.author.username}`);
+                    console.log(`  | Code: ${verCode}`);
+                }
             });
+
+            // Tell the user to check their email
+            message.author.send('Please check your email for a verification code');
+            break;
+        }
+
+        // Check if not found and send message
+        if (i == cols[2].rowData.length) message.author.send(data.regNotFound);
     });
 };
+
+/**
+ * Returns the cell value given row and column of a sheets column object
+ * @param {object} cols The Google API column object
+ * @param {number} row Row number
+ * @param {number} col Column number
+ * @returns {string} The cell value, as a string
+ */
+const cellValue = (cols, row, col) => cols[col].rowData[row].values[0].formattedValue;
