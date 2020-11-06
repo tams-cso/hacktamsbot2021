112242eb689e00ce3ad89b87b3c27ef8cf7a8b80
Michael Zhao <michaelzhao314@gmail.com>
2020-11-06 01:53:03 -0600

    Extract prefix/events to data.json + events format UTC/ISO

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
+const data = require('./data.json');
+
 const client = new Discord.Client({ ws: { intents: Discord.Intents.ALL } });
-const prefix = '!';
+
 const roles = {
     team: '750894547462127686',
     hacker: '763118079918997504',
     mentor: '763118116779458590'
 };
-const events = {
-    1: ['Opening Ceremony','Friday','11-06','18:00','<https://hacktams.org/twitch>'],
-    2: ['Team Formations','Friday','11-06','18:45','event-room Voice Channel on Discord'],
-    3: ['Basics to Hackathons','Friday','11-06','21:00','<https://hacktams.org/zoom>'],
-    4: ['Intro to Git + Build a Portfolio Website Workshop','Friday','11-06','21:15','<https://hacktams.org/zoom>'],
-    5: ['Game Night with Hackers','Friday','11-06','22:30','event-room Voice Channel on Discord'],
-    6: ['Tools & Techniques for Machine Learning Workshop','Saturday','11-07','9:00','<https://hacktams.org/zoom>'],
-    7: ['Stunning by Design - UI/UX Workshop','Saturday','11-07','10:00','<https://hacktams.org/zoom>'],
-    8: ['Build a Clean Web App in Minutes with React - React.JS Workshop','Saturday','11-07','11:30','<https://hacktams.org/zoom>'],
-    9: ['Going Deep into Deep Learning - PyTorch Workshop','Saturday','11-07','13:00','<https://hacktams.org/zoom>'],
-    10: ['How to Build a Cloud-Connected AR/VR App in 15 Minutes or Less - echoAR Workshop','Saturday','11-07','14:00','<https://meet.google.com/dez-aohx-sin>'],
-    11: ['Q&A with Brian Truong: Founder of 2 VC-backed startups','Saturday','11-07','15:30','<https://hacktams.org/zoom>'],
-    12: ['Panel of UTD’s Society of Women Engineers Officers','Saturday','11-07','17:00','<https://hacktams.org/zoom>'],
-    13: ['Graphics with OpenGL Workshop','Saturday','11-07','18:00','<https://hacktams.org/zoom>'],
-    14: ['Game Night with Hackers','Saturday','11-07','20:00','event-room Voice Channel on Discord'],
-    15: ['MS Painting with Bob Ross','Saturday','11-07','23:00','event-room Voice Channel on Discord'],
-    16: ['Project Submission 101 Workshop','Sunday','11-08','12:00','<https://hacktams.org/zoom>'],
-    17: ['Live Demos','Sunday','11-08','15:00','<https://hacktams.org/twitch>'],
-    18: ['Closing Ceremony','Sunday','11-08','18:00','<https://hacktams.org/twitch>']
-};
+
 const servers = {
     testing: '754426418208964610',
     hackTAMS: '750894174966120558'
@@ -62,7 +45,7 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if (message.channel.type == 'dm' && !message.author.bot) {
-        if (!message.content.startsWith(prefix)) {
+        if (!message.content.startsWith(data.prefix)) {
             const args = message.content.toLowerCase().split(/\s+/);
             if (args.length == 3 && args[2].match(/.+@.+\..+/) != -1) {
                 var done = false;
@@ -121,7 +104,7 @@ client.on('message', message => {
                 break;
             case 'evtgen':
                 for (var i = 1; i < 19; i++) {
-                    let item = events[i];
+                    let item = data.events[i];
                     message.channel.send(`${item[0]}\n${item[1]} ${item[2]} ${item[3]}\nLocation: ${item[4]}`).then(message.react(':thumbsup:'));
                 }
                 break;
@@ -129,9 +112,9 @@ client.on('message', message => {
         return;
     }
 
-    if (!message.content.startsWith(prefix) || message.author.bot)
+    if (!message.content.startsWith(data.prefix) || message.author.bot)
         return;
-    const args = message.content.slice(prefix.length).split(/\s+/)
+    const args = message.content.slice(data.prefix.length).split(/\s+/)
     const command = args.shift();
     message.delete();
 
